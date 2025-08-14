9f25924c6aa339aa22f16b6575aab6f4c153f7e8
Yénifer Gómez Sanz <yenifer.gomez.sanz@tajamar365.com>
2025-08-14 18:19:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const acceptButton = document.getElementById('accept-cookies');
     const denyButton = document.getElementById('deny-cookies');
 
-    // Comprobar si el usuario ya ha aceptado las cookies
     if (localStorage.getItem('cookies-accepted') === 'true') {
         cookieModalOverlay.style.display = 'none';
     } else {
         cookieModalOverlay.style.display = 'flex';
     }
 
-    // Ocultar el modal al hacer clic en "Aceptar"
     acceptButton.addEventListener('click', () => {
         localStorage.setItem('cookies-accepted', 'true');
         cookieModalOverlay.style.display = 'none';
     });
     
-    // Ocultar el modal al hacer clic en "Denegar"
     denyButton.addEventListener('click', () => {
         cookieModalOverlay.style.display = 'none';
     });
