8ce91e012189fa080d21fd36080926e6cfd8e66f
Yénifer Gómez Sanz <yenifer.gomez.sanz@tajamar365.com>
2025-08-17 22:31:28 +0200

    Update tpl-footer.js

--- tpl-footer.js
+++ tpl-footer.js
@@ -1,5 +1,3 @@
-<!-- TPL: INICIO tpl-footer.js -->
-<script>
 (function () {
   function setYear() {
     document.querySelectorAll('.year-span').forEach(function (s) {
@@ -7,36 +5,27 @@
     });
   }
 
-  function tryFetch(paths) {
-    // Intenta varias rutas hasta que una funcione
-    return paths.reduce(function (chain, path) {
-      return chain.catch(function () {
-        return fetch(path, { cache: 'no-store' }).then(function (r) {
-          if (!r.ok) throw new Error('HTTP '+r.status+' en '+path);
-          return r.text();
-        });
-      });
-    }, Promise.reject());
-  }
-
   function injectFooter() {
     var mount = document.getElementById('tpl-footer');
     if (!mount) return;
 
-    // Rutas robustas:
-    //  - 'tpl-footer.html' -> para páginas en raíz
-    //  - './tpl-footer.html' -> relativo seguro
-    //  - '/tpl-footer.html' -> dominio raíz (útil si usas dominio propio como www.thepetslovers.es)
     var paths = ['tpl-footer.html', './tpl-footer.html', '/tpl-footer.html'];
 
-    tryFetch(paths)
-      .then(function (html) {
-        mount.outerHTML = html;
-        setYear();
-      })
-      .catch(function (err) {
-        console.error('TPL FOOTER: No se pudo cargar el footer.', err);
+    paths.reduce(function(chain, path){
+      return chain.catch(function(){
+        return fetch(path, { cache: 'no-store' }).then(function(res){
+          if(!res.ok) throw new Error('HTTP '+res.status+' en '+path);
+          return res.text();
+        });
       });
+    }, Promise.reject())
+    .then(function (html) {
+      mount.outerHTML = html;
+      setYear();
+    })
+    .catch(function (err) {
+      console.error('TPL FOOTER: No se pudo cargar el footer.', err);
+    });
   }
 
   if (document.readyState === 'loading') {
@@ -45,5 +34,3 @@
     injectFooter();
   }
 })();
-</script>
-<!-- TPL: FIN tpl-footer.js -->
