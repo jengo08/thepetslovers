ec563f62f069128d5ffb9d39f994e26fea0f1a4d
Yénifer Gómez Sanz <yenifer.gomez.sanz@tajamar365.com>
2025-08-17 22:07:02 +0200

    Update tpl-footer.js

--- tpl-footer.js
+++ tpl-footer.js
@@ -7,18 +7,35 @@
     });
   }
 
+  function tryFetch(paths) {
+    // Intenta varias rutas hasta que una funcione
+    return paths.reduce(function (chain, path) {
+      return chain.catch(function () {
+        return fetch(path, { cache: 'no-store' }).then(function (r) {
+          if (!r.ok) throw new Error('HTTP '+r.status+' en '+path);
+          return r.text();
+        });
+      });
+    }, Promise.reject());
+  }
+
   function injectFooter() {
     var mount = document.getElementById('tpl-footer');
     if (!mount) return;
 
-    fetch('tpl-footer.html', { cache: 'no-store' })
-      .then(function (res) { return res.text(); })
+    // Rutas robustas:
+    //  - 'tpl-footer.html' -> para páginas en raíz
+    //  - './tpl-footer.html' -> relativo seguro
+    //  - '/tpl-footer.html' -> dominio raíz (útil si usas dominio propio como www.thepetslovers.es)
+    var paths = ['tpl-footer.html', './tpl-footer.html', '/tpl-footer.html'];
+
+    tryFetch(paths)
       .then(function (html) {
         mount.outerHTML = html;
         setYear();
       })
       .catch(function (err) {
-        console.error('TPL: Error al cargar el footer:', err);
+        console.error('TPL FOOTER: No se pudo cargar el footer.', err);
       });
   }
 
